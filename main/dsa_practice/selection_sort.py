ee8ec3043e125730f8cbd4d505e3fee299bb6a24
Tanusree-Das <tnsrdas@gmail.com>
2025-01-17 07:26:55 +0000

    code change

--- main/dsa_practice/selection_sort.py
+++ main/dsa_practice/selection_sort.py
@@ -7,3 +7,17 @@ How it works:
 	•	Move the lowest value to the front of the unsorted part of the array.
 	•	Go through the array again as many times as there are values in the array.
 '''
+
+def selection_sort(arr):
+    arr_range=len(arr)-1
+    for i in range(arr_range):
+        lowest_value_index = i
+        for j in range(i,arr_range+1,1):
+            if arr[j]<arr[lowest_value_index]:
+                lowest_value_index = j
+        arr[i],arr[lowest_value_index]=arr[lowest_value_index],arr[i]
+    return arr
+
+if __name__=="__main__":
+    array_to_sort=[10,11,8]
+    print(selection_sort(array_to_sort))
