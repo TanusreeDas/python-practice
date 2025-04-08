e16994df894ea008dca2e00b6868170ccb547f67
Tanusree-Das <tnsrdas@gmail.com>
2025-04-08 19:19:03 +0100

    Leet code 242

--- main/random_problem_solve/5_find_duplicate.py
+++ main/random_problem_solve/5_find_duplicate.py
@@ -1,15 +1,16 @@
 from  collections import Counter
 from typing import List
 class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
+    @staticmethod
+    def has_duplicate(self, nums: List[int]) -> bool:
         if len(nums)>0:
             d = Counter(nums)
-            max_occurance = d.most_common(1)[0][1]
-            if max_occurance>1:
+            max_occurrence = d.most_common(1)[0][1]
+            if max_occurrence>1:
                 return True
         return False
 
 
 if __name__ == "__main__":
     sol= Solution()
-    print(sol.hasDuplicate([]))
+    print(sol.has_duplicate([]))
