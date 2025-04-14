9af189baed5b8ccb0c9df0fc91ce16857667c7a8
Tanusree-Das <tnsrdas@gmail.com>
2025-04-14 14:47:48 +0100

    Leetcode#217

--- main/neet_code_150/1_find_duplicate.py
+++ main/neet_code_150/1_find_duplicate.py
@@ -1,4 +1,6 @@
-'''Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.'''
+'''Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+Best Approach: 1. Hash Set (O(n))
+'''
 
 from  collections import Counter
 
