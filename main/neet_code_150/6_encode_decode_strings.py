7da14c64326b44f0fac373f77a8f5a01476411eb
Tanusree-Das <tnsrdas@gmail.com>
2025-04-14 14:49:34 +0100

    lintcode#659-premium leet code problem (also avl in lint code)

--- main/neet_code_150/6_encode_decode_strings.py
+++ main/neet_code_150/6_encode_decode_strings.py
@@ -1,3 +1,13 @@
+'''
+Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+Constraints:
+0 <= strs.length < 100
+0 <= strs[i].length < 200
+strs[i] contains only UTF-8 characters.
+
+Best Approach: Without find O(n)
+'''
+
 class Solution:
 
     @staticmethod
